5b5311388a8c35ef0639c4927fa9d53b8c0e869d
biteytech <biteytech@protonmail.com>
2021-10-04 01:31:24 -0700

    fixed maven deploy

---
+++ dataframe/dataframe-test/src/main/java/tech/bitey/dataframe/test/Placeholder.java
@@ -0,0 +1,9 @@
+package tech.bitey.dataframe.test;
+
+/**
+ * This module only contains unit tests. We need this placeholder class to force
+ * creation of a Javadoc jar as required by Maven Central.
+ */
+public class Placeholder {
+
+}
